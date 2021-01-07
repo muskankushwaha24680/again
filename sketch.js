25b525173e547a0cfe6da31359a76c3bd37234c4
Muskan Kushwaha <73283486+muskankushwaha24680@users.noreply.github.com>
2021-01-07 16:45:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+//Create variables here
+var dog , dogImg , happydogImg , database , foodS , foodStock ;
+var foodStock = 20 ;
+
+function preload()
+{
+  //load images here
+  dogImg = loadImage("Dog.png");
+  happydogImg = loadImage("happydog.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  
+  dog = createSprite(250,300,50,70);
+  dog.addImage(dogImg);
+  dog.scale = 0.3;
+
+  database = firebase.database();
+  foodStock = database.ref('Food');
+  foodStock.on("value" , readStock);
+}
+
+
+function draw() {  
+background(46,139,87);
+
+textSize(15);
+stroke("black");
+fill("white");
+text(" Note: Press UP_ARROW Key To Feed Drago Milk ", 50,50);
+
+if(keyWentDown (UP_ARROW)){
+  writeStock(foodS);
+  dog.addImage(happydogImg);
+}
+
+
+
+  drawSprites();
+  //add styles here
+  textSize(20);
+  fill("red");
+text("foodRemaining :"+  foodS , 100,150);
+}
+
+function readStock(data){
+foodS = data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x=0
+    }
+    else{
+      x = x - 1 ;
+    }
+database.ref('/').update({
+  Food: x
+})
+
+}
